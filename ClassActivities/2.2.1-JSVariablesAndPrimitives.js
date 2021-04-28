3447e569807335ec90cf1306c5ca83478a1b6efe
Gardgnome <gardgnome@gmail.com>
2021-04-28 12:55:07 -0400

    .js

---
+++ ClassActivities/2.2.1-JSVariablesAndPrimitives.js
@@ -0,0 +1,24 @@
+// create a constant variable (const)
+// const name = "Aaron";
+//console.log(name);
+// create a variable that can be reassigned (let)
+let age = 36
+console.log(age);
+// create three variables and assign them values of different data types (=)
+let house = "gray";
+let pet = 2;
+let style = "Be Happy";
+// print the types of two variables that reference two different data types (typeof)
+// TIP: console.log() prints a value in the console/terminal
+console.log(typeof house);
+console.log(typeof pet);
+console.log(typeof style);
+// change the value referenced by a variable (dynamic typing)
+pet = "Sophia";
+// print the type of the variable you just changed (typeof)
+console.log(typeof pet)
+// create variables and give them values to complete the sentences that will print
+const name = "AARon";
+let numberOfWeeks = 3;
+
+console.log("Hello, my name is " + name + "and I have been learning Javascript for " + numberOfWeeks + " weeks");
