e8e2379148f6486936685428a87280b77b315a66
Gardgnome <gardgnome@gmail.com>
2021-04-28 14:51:42 -0400

    2.2.2

---
+++ ClassActivities/2.2.2-JS.js
@@ -0,0 +1,35 @@
+// create a variable called "name" that references a string
+let name = "Aaron";
+// create a variable called "favoriteSong" that references a string
+let favoriteSong = "Head like a Hole";
+// create a variable called "wage" that references a number
+let wage = 8;
+// create a variable called "age" that references a number
+let age = 36;
+// create a variable called "onlyChild" that references a boolean
+let onlyChild = false;
+// create a variable called "satisfied" that references a boolean
+let satisfied = false;
+// create a variable called "favoriteThing" that references ANY primitive value
+// print the data type of "favoriteThing"
+let favoriteThing = 'dogs';
+// ! You heard a new song that became your favorite.
+// Reassign the variable "favoriteSong"
+favoriteSong = 'Thriller';
+// ! You no longer like the type of data stored in "favoriteThing"
+// Reassign "favoriteThing" to a different primitive data type and print its type
+favoriteThing = 'cats';
+// ! Happy Birthday!
+// Reassign "age" to a new value
+age = 25;
+
+// ! You got a big raise at work.
+// Reassign "wage" to a new value
+wage = 88;
+
+// ! Your raise changed your job satisfaction.
+// Assign "satisfied" to true
+satisfied = true;
+
+// use "console.log()" and "+" to print a sentence that includes two variables
+console.log("Hi, my name is " + name + ". I am " + age + ". My fav song is " + favoriteSong);
